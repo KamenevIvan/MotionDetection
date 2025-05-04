711cfdc8daa763e3bd1695591cb3cf5cf584ac74
ufimtsevv <t.ufimtsev@g.nsu.ru>
2025-05-04 18:38:49 +0700

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -1,6 +1,5 @@
 ######## detector settings (global variables) ############
 
-buffer_size = 30
 nf_object = 3000          # maximum number of frames for object to be tracked = length of detections CL 
 background_history = 100    # number of frames in background subtractor history 
 iou_threshold_id = 0.2     # threshold for iou between consequtive frames for object to tracked (0 .. 1.0) 
@@ -16,14 +15,25 @@ check_night_per_frames = 10
 update_background_per_frames = 10
 
 enable_print_console = False
-enable_output_file = False
-enable_show_boxes = False
+enable_output_file = True
+enable_show_boxes = True
 enable_save_frames = False
 
-inputfile = "vki.avi"  
+ENABLE_ANNOTATION_OPTIMIZATION = False
+IOU_THRESHOLD = 0.5
+
+inputfile = r"C:\uni\project\TLP\PolarBear3\PolarBear3.mp4"
 outputfile = 'mot-results.txt'
-output_frame_dir = r"F:\VScode\NSU\DetectMotion\VKI_CAMERAS_TESTS\magadan\frames"
+output_frame_dir = r"C:\uni\project\MotionDetection\VKI_CAMERAS_TESTS"
 box_color = (0, 0, 255)
 
-if buffer_size < nf_object:
-    buffer_size = nf_object
+######## optimized parameters ############
+
+color_space = 'YCbCr'
+threshold_y = 10
+threshold_chroma = 38
+blur = 15
+minimum_are_contours = 60
+merge_distance_threshold = 11
+overlap_threshold = 0.322082618487802
+buffer_size = 3000
